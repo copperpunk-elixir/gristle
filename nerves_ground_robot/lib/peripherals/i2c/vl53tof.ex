fa7c87ad6051b13159b20f57191a2e6cef8766f8
Greg Gradwell <greghgradwell@live.com>
2020-02-18 21:25:56 -0800

    Finished VL53L1X interface. Ready to be implemented with GenServer.

--- nerves_ground_robot/lib/peripherals/i2c/vl53tof.ex
+++ nerves_ground_robot/lib/peripherals/i2c/vl53tof.ex
@@ -7,8 +7,14 @@ defmodule Peripherals.I2c.Vl53tof do
   @default_address 0x29
   @system_mode_start 0x0087
   @vhv_config 0x0008
+  @range_config_A_HI 0x005E
+  @range_config_B_HI 0x0062
+  @osc_calibrate 0x00DE
+  @intermeasurement_period 0x006C
 
-  def new_vl53tof(config) do
+
+  # ----- BEGIN MANDATORY INTERFACE -----
+  def new_sensor(config) do
     Logger.debug("Start VL53ToF")
     bus_ref = Peripherals.I2c.Utils.get_bus_ref(Map.get(config, :i2c_bus, @default_bus))
     address = @default_address
@@ -16,102 +22,16 @@ defmodule Peripherals.I2c.Vl53tof do
   end
 
   def begin(device) do
-    default_config = [
-      0x00, # 0x2d : set bit 2 and 5 to 1 for fast plus mode (1MHz I2C), else don't touch */
-      0x01, # 0x2e : bit 0 if I2C pulled up at 1.8V, else set bit 0 to 1 (pull up at AVDD) */
-      0x01, # 0x2f : bit 0 if GPIO pulled up at 1.8V, else set bit 0 to 1 (pull up at AVDD) */
-      0x01, # 0x30 : set bit 4 to 0 for active high interrupt and 1 for active low (bits 3:0 must be 0x1), use SetInterruptPolarity() */
-      0x02, # 0x31 : bit 1 = interrupt depending on the polarity, use CheckForDataReady() */
-      0x00, # 0x32 : not user-modifiable */
-      0x02, # 0x33 : not user-modifiable */
-      0x08, # 0x34 : not user-modifiable */
-      0x00, # 0x35 : not user-modifiable */
-      0x08, # 0x36 : not user-modifiable */
-      0x10, # 0x37 : not user-modifiable */
-      0x01, # 0x38 : not user-modifiable */
-      0x01, # 0x39 : not user-modifiable */
-      0x00, # 0x3a : not user-modifiable */
-      0x00, # 0x3b : not user-modifiable */
-      0x00, # 0x3c : not user-modifiable */
-      0x00, # 0x3d : not user-modifiable */
-      0xff, # 0x3e : not user-modifiable */
-      0x00, # 0x3f : not user-modifiable */
-      0x0F, # 0x40 : not user-modifiable */
-      0x00, # 0x41 : not user-modifiable */
-      0x00, # 0x42 : not user-modifiable */
-      0x00, # 0x43 : not user-modifiable */
-      0x00, # 0x44 : not user-modifiable */
-      0x00, # 0x45 : not user-modifiable */
-      0x20, # 0x46 : interrupt configuration 0->level low detection, 1-> level high, 2-> Out of window, 3->In window, 0x20-> New sample ready , TBC */
-    0x0b, # 0x47 : not user-modifiable */
-    0x00, # 0x48 : not user-modifiable */
-    0x00, # 0x49 : not user-modifiable */
-    0x02, # 0x4a : not user-modifiable */
-    0x0a, # 0x4b : not user-modifiable */
-    0x21, # 0x4c : not user-modifiable */
-    0x00, # 0x4d : not user-modifiable */
-    0x00, # 0x4e : not user-modifiable */
-    0x05, # 0x4f : not user-modifiable */
-    0x00, # 0x50 : not user-modifiable */
-    0x00, # 0x51 : not user-modifiable */
-    0x00, # 0x52 : not user-modifiable */
-    0x00, # 0x53 : not user-modifiable */
-    0xc8, # 0x54 : not user-modifiable */
-    0x00, # 0x55 : not user-modifiable */
-    0x00, # 0x56 : not user-modifiable */
-    0x38, # 0x57 : not user-modifiable */
-    0xff, # 0x58 : not user-modifiable */
-    0x01, # 0x59 : not user-modifiable */
-    0x00, # 0x5a : not user-modifiable */
-    0x08, # 0x5b : not user-modifiable */
-    0x00, # 0x5c : not user-modifiable */
-    0x00, # 0x5d : not user-modifiable */
-    0x01, # 0x5e : not user-modifiable */
-    0xdb, # 0x5f : not user-modifiable */
-    0x0f, # 0x60 : not user-modifiable */
-    0x01, # 0x61 : not user-modifiable */
-    0xf1, # 0x62 : not user-modifiable */
-    0x0d, # 0x63 : not user-modifiable */
-    0x01, # 0x64 : Sigma threshold MSB (mm in 14.2 format for MSB+LSB), use SetSigmaThreshold(), default value 90 mm  */
-    0x68, # 0x65 : Sigma threshold LSB */
-    0x00, # 0x66 : Min count Rate MSB (MCPS in 9.7 format for MSB+LSB), use SetSignalThreshold() */
-    0x80, # 0x67 : Min count Rate LSB */
-    0x08, # 0x68 : not user-modifiable */
-    0xb8, # 0x69 : not user-modifiable */
-    0x00, # 0x6a : not user-modifiable */
-    0x00, # 0x6b : not user-modifiable */
-    0x00, # 0x6c : Intermeasurement period MSB, 32 bits register, use SetIntermeasurementInMs() */
-    0x00, # 0x6d : Intermeasurement period */
-    0x0f, # 0x6e : Intermeasurement period */
-    0x89, # 0x6f : Intermeasurement period LSB */
-    0x00, # 0x70 : not user-modifiable */
-    0x00, # 0x71 : not user-modifiable */
-    0x00, # 0x72 : distance threshold high MSB (in mm, MSB+LSB), use SetD:tanceThreshold() */
-    0x00, # 0x73 : distance threshold high LSB */
-    0x00, # 0x74 : distance threshold low MSB ( in mm, MSB+LSB), use SetD:tanceThreshold() */
-    0x00, # 0x75 : distance threshold low LSB */
-    0x00, # 0x76 : not user-modifiable */
-    0x01, # 0x77 : not user-modifiable */
-    0x0f, # 0x78 : not user-modifiable */
-    0x0d, # 0x79 : not user-modifiable */
-    0x0e, # 0x7a : not user-modifiable */
-    0x0e, # 0x7b : not user-modifiable */
-    0x00, # 0x7c : not user-modifiable */
-    0x00, # 0x7d : not user-modifiable */
-    0x02, # 0x7e : not user-modifiable */
-    0xc7, # 0x7f : ROI center, use SetROI() */
-    0xff, # 0x80 : XY ROI (X=Width, Y=Height), use SetROI() */
-    0x9B, # 0x81 : not user-modifiable */
-    0x00, # 0x82 : not user-modifiable */
-    0x00, # 0x83 : not user-modifiable */
-    0x00, # 0x84 : not user-modifiable */
-    0x01, # 0x85 : not user-modifiable */
-    0x00, # 0x86 : clear interrupt, use ClearInterrupt() */
-    0x00  # 0x87 : start ranging, use StartRanging() or StopRanging(), If you want an automatic start after VL53L1X_init() call, put 0x40 in location 0x87 */
-    ]
+    default_config =
+      [0x00,0x01,0x01,0x01,0x02,0x00,0x02,0x08,0x00,0x08,0x10,0x01,0x01,0x00,0x00,0x00,0x00,
+       0xff,0x00,0x0F,0x00,0x00,0x00,0x00,0x00,0x20,0x0b,0x00,0x00,0x02,0x0a,0x21,0x00,0x00,
+       0x05,0x00,0x00,0x00,0x00,0xc8,0x00,0x00,0x38,0xff,0x01,0x00,0x08,0x00,0x00,0x01,0xdb,
+       0x0f,0x01,0xf1,0x0d,0x01,0x68,0x00,0x80,0x08,0xb8,0x00,0x00,0x00,0x00,0x0f,0x89,0x00,
+       0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x0f,0x0d,0x0e,0x0e,0x00,0x00,0x02,0xc7,0xff,0x9B,
+       0x00,0x00,0x00,0x01,0x00,0x00]
     start_register = 0x2D
     Enum.reduce(default_config, start_register, fn (byte, register) ->
-      Logger.debug("byte/register: #{byte}/#{register}")
+      # Logger.debug("byte/register: #{byte}/#{register}")
       write_byte_to_register(device, register, byte)
       Process.sleep(1)
       register+1
@@ -121,21 +41,94 @@ defmodule Peripherals.I2c.Vl53tof do
    write_byte_to_register(device, 0x0B, 0)
   end
 
-  def start_ranging(device) do
+  def set_distance_mode_short(device) do
+    set_distance_mode(device, 1)
+  end
+
+  def set_distance_mode_long(device) do
+    set_distance_mode(device, 2)
+  end
+
+  def set_update_interval_ms(device, interval_ms) do
+    # Always set the timing budget equal to the measurement interval
+    actual_timing_interval_ms = set_timing_budget_ms(device, interval_ms)
+    Process.sleep(10)
+    clock_pll = read_packet_at_register(device, @osc_calibrate, 2)
+    |> word_to_value()
+    |> Bitwise.&&&(0x03FF)
+    clock_pll_value = floor(clock_pll*actual_timing_interval_ms*1.075)
+    clock_packet = [
+    (clock_pll_value >>> 24) &&& 0xFF,
+    (clock_pll_value >>> 16) &&& 0xFF,
+    (clock_pll_value >>> 8) &&& 0xFF,
+    clock_pll_value &&& 0xFF]
+    write_packet_to_register(device, @intermeasurement_period, clock_packet)
+  end
+
+  def start_measurements(device) do
     write_byte_to_register(device, @system_mode_start, 0x40)
   end
 
-  def stop_ranging(device) do
+  def stop_measurements(device) do
     write_byte_to_register(device, @system_mode_start, 0x00)
   end
 
+  def get_distance(device) do
+    get_distance_mm(device) / 1000
+  end
+
+  # ----- END MANDATORY INTERFACE -----
+
+  # ----- BEGIN OPTIONAL INTERFACE -----
+  def get_distance_mode(device) do
+    temp_distance_mode = read_byte_at_register(device, 0x004B)
+    # Logger.debug("temp dm: #{temp_distance_mode}")
+    case temp_distance_mode do
+      0x14 ->
+        Logger.debug("Short range mode")
+        1
+      0x0A ->
+        Logger.debug("Long range mode")
+        2
+      _ ->
+        Logger.error("Unknown distance mode")
+        nil
+    end
+  end
+
+  def get_single_distance(device) do
+    start_measurements(device)
+    wait_for_data(device, 100)
+    distance = get_distance(device)
+    clear_interrupt(device)
+    stop_measurements(device)
+    distance
+  end
+
+  def get_update_interval_ms(device) do
+    timing_budget_temp = read_packet_at_register(device, @range_config_A_HI, 2)
+    |> word_to_value()
+    case timing_budget_temp do
+      0x001D -> 15
+      0x001E -> 20
+      0x0060 -> 33
+      0x00AD -> 50
+      0x01CC -> 100
+      0x02D9 -> 200
+      0x048F -> 500
+      _ -> 0
+    end
+  end
+
+  # ----- END OPTIONAL INTERFACE -----
+
   defp confirm_sensor_active(device, count) do
     if count == 0 do
-      start_ranging(device)
+      start_measurements(device)
     end
     if is_data_ready?(device) do
       clear_interrupt(device)
-      stop_ranging(device)
+      stop_measurements(device)
     else
       if (count > 50) do
         raise "VL53ToF is not responding"
@@ -153,20 +146,20 @@ defmodule Peripherals.I2c.Vl53tof do
 
   defp get_interrupt_polarity(device) do
     result = read_byte_at_register(device, 0x0030)
-    Logger.debug("GIP result0: #{result}")
+    # Logger.debug("GIP result0: #{result}")
     result = result &&& 0x10
-    Logger.debug("GIP result0.5: #{result}")
+    # Logger.debug("GIP result0.5: #{result}")
     result = result >>> 4
-    Logger.debug("GIP result1: #{result}")
+    # Logger.debug("GIP result1: #{result}")
     result = Bitwise.bxor(result, 1)
-    Logger.debug("GIP result2: #{result}")
+    # Logger.debug("GIP result2: #{result}")
     result
   end
 
-  def is_data_ready?(device) do
+  defp is_data_ready?(device) do
     interrupt_polarity = get_interrupt_polarity(device)
     hv_status = read_byte_at_register(device, 0x31)
-    Logger.debug("HV_status: #{hv_status}")
+    # Logger.debug("HV_status: #{hv_status}")
     if (hv_status &&& 1) == interrupt_polarity do
       true
     else
@@ -174,7 +167,7 @@ defmodule Peripherals.I2c.Vl53tof do
     end
   end
 
-  def wait_for_data(device, timeout) do
+  defp wait_for_data(device, timeout) do
     wait_for_data(device, 0, timeout)
   end
 
@@ -189,39 +182,83 @@ defmodule Peripherals.I2c.Vl53tof do
     end
   end
 
-  def get_distance_mode(device) do
-    temp_distance_mode = read_byte_at_register(device, 0x004B)
-    Logger.debug("temp dm: #{temp_distance_mode}")
-    case temp_distance_mode do
-      0x14 -> 1
-      0x0A -> 2
-      _ ->
-        Logger.error("Unknown distance mode")
-        nil
+  defp set_timing_budget_ms(device, interval_ms) do
+    distance_mode = get_distance_mode(device)
+    {timing_word_1, timing_word_2, actual_interval_ms} =
+      case distance_mode do
+        1 ->
+          Logger.debug("Set timing budget for short range")
+          cond do
+            interval_ms < 20 -> {0x001D, 0x0027, 15} # 15ms
+            interval_ms < 33 -> {0x0051, 0x006E, 20} # 20ms
+            interval_ms < 50 -> {0x00D6, 0x006E, 33} # 33ms
+            interval_ms < 100 -> {0x01AE, 0x1E8, 50} # 50ms
+            interval_ms < 200 -> {0x02E1, 0x0388, 100} # 100ms
+            interval_ms < 500 -> {0x03E1, 0x0496, 200} # 200ms
+            true -> {0x0591, 0x05C1, 500} # 500ms
+          end
+        2 ->
+          Logger.debug("Set timing budget for long range")
+          cond do
+            interval_ms < 33 -> {0x001E, 0x0022, 20} # 20ms
+            interval_ms < 50 -> {0x0060, 0x006E, 33} # 33ms
+            interval_ms < 100 -> {0x00AD, 0x00C6, 50} # 50ms
+            interval_ms < 200 -> {0x01CC, 0x01EA, 100} # 100ms
+            interval_ms < 500 -> {0x02D9, 0x02F8, 200} # 200ms
+            true -> {0x048F, 0x04A4, 500} # 500ms
+          end
+        _ -> {nil, nil, nil}
+      end
+    unless actual_interval_ms == nil do
+      write_timing_budget_packets(device, timing_word_1, timing_word_2)
     end
+    actual_interval_ms
   end
 
-  def get_single_distance(device) do
-    start_ranging(device)
-    wait_for_data(device, 100)
-    distance = get_distance(device)
-    clear_interrupt(device)
-    stop_ranging(device)
-    distance
+  defp write_timing_budget_packets(device, word1, word2) do
+    packet1 = get_packet_for_word(word1)
+    packet2 = get_packet_for_word(word2)
+    write_packet_to_register(device, @range_config_A_HI, packet1)
+    write_packet_to_register(device, @range_config_B_HI, packet2)
   end
 
-  def get_distance(device) do
-    result = read_packet_at_register(device, 0x0096, 2)
-    Logger.debug("result: #{inspect(result)}")
-    msb = Enum.at(result, 0)
-    lsb = Enum.at(result, 1)
-    Logger.debug("msb/lsb: #{msb}/#{lsb}")
-    (msb <<< 8) + lsb
+  defp set_distance_mode(device, distance_mode) do
+    register_list_bytes = [0x004B, 0x0060, 0x0063, 0x0069]
+    register_list_words = [0x0078, 0x007A]
+    {byte_list, word_list} =
+      case distance_mode do
+        1 -> {[0x14, 0x07, 0x05, 0x38], [0x0705, 0x0606]}
+        2 -> {[0x0A, 0x0F, 0x0D, 0xB8], [0x0F0D, 0x0E0E]}
+        _ -> {nil, nil}
+      end
+     unless (byte_list == nil) do
+       Enum.each(0..length(register_list_bytes)-1, fn index ->
+         write_byte_to_register(device, Enum.at(register_list_bytes,index), Enum.at(byte_list, index))
+       end)
+       Enum.each(0..length(register_list_words)-1, fn index ->
+         send_packet = get_packet_for_word(Enum.at(word_list, index))
+         write_packet_to_register(device, Enum.at(register_list_words,index), send_packet)
+       end)
+     end
+  end
+
+  defp get_distance_mm(device) do
+    read_packet_at_register(device, 0x0096, 2)
+    |> word_to_value()
+    # Logger.debug("result: #{inspect(result)}")
+    # msb = Enum.at(result, 0)
+    # lsb = Enum.at(result, 1)
+    # Logger.debug("msb/lsb: #{msb}/#{lsb}")
+    # (msb <<< 8) + lsb
   end
 
   defp write_byte_to_register(device, register, data_byte) do
-    packet = [register >>> 8, register &&& 0xFF, data_byte]
-    Peripherals.I2c.Utils.write_packet(device.bus_ref, device.address, packet)
+    write_packet_to_register(device, register, [data_byte])
+  end
+
+  defp write_packet_to_register(device, register, packet) do
+    send_packet = [register >>> 8, register &&& 0xFF] ++ packet
+    Peripherals.I2c.Utils.write_packet(device.bus_ref, device.address, send_packet)
   end
 
   defp read_byte_at_register(device, register) do
@@ -233,5 +270,13 @@ defmodule Peripherals.I2c.Vl53tof do
     send_packet = [register >>> 8, register &&& 0xFF]
     Peripherals.I2c.Utils.write_packet(device.bus_ref, device.address, send_packet)
     Peripherals.I2c.Utils.read_packet(device.bus_ref, device.address, num_bytes)
+  end
+
+  defp word_to_value(word) do
+    (Enum.at(word, 0) <<< 8) + Enum.at(word, 1)
+  end
+
+  defp get_packet_for_word(word) do
+    [word >>> 8, word &&& 0xFF]
   end
 end
