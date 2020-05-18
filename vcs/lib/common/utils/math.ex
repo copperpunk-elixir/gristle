19bccaa8bff5dbb6bcee1b2437b0c27b27e7d472
Greg Gradwell <greghgradwell@live.com>
2020-05-18 15:46:06 -0700

    fp_from_uint calculates floating-point value for uint32 or uint64

--- vcs/lib/common/utils/math.ex
+++ vcs/lib/common/utils/math.ex
@@ -1,4 +1,5 @@
 defmodule Common.Utils.Math do
+  require Bitwise
 
   def constrain(x, min_value, max_value) do
     case x do
@@ -25,4 +26,52 @@ defmodule Common.Utils.Math do
       x*acc
     end)
   end
+
+  def fp_from_uint(x, bits) do
+    {sig_start,exp_min_index,exp_max_index, exp_subtract,significand_div, exp_and} =
+      case bits do
+        32 -> {0x7FFFFF,23,30,127, 8388608, 0x100}
+        64 -> {0xFFFFFFFFFFFFF,52,62,1023, 0x10000000000000, 0x800}
+      end
+    # significand = Bitwise.<<<(1,exp_min_index) -1
+    significand =Bitwise.&&&(sig_start,x)
+    exponent = Bitwise.>>>(x,exp_min_index)
+    IO.puts("exp: #{exponent}")
+    # exponent = exponent - Bitwise.&&&(exponent,Bitwise.<<<(1,exp_max_index-exp_min_index+1)) - exp_subtract
+    exponent = exponent - Bitwise.&&&(exponent,exp_and) - exp_subtract
+
+    # IO.puts("exp: #{exponent}")
+    # exponent = exponent - exp_subtract
+    IO.puts("exp: #{exponent}")
+    sign = if Bitwise.>>>(x,bits-1) == 1 do
+      -1
+    else
+      1
+    end
+    IO.puts("sign: #{sign}")
+    IO.puts("exponent: #{exponent}")
+    IO.puts("significand: #{significand}")
+    exp_mult =
+    if exponent > 0 do
+      Bitwise.<<<(1,exponent)
+    else
+      1/Bitwise.<<<(1,-exponent)
+    end
+    IO.puts("exp mult: #{exp_mult}")
+    value = sign*(1+significand/significand_div)*exp_mult
+
+    IO.puts("value: #{value}")
+
+  end
+
+  def twos_comp_32(x) do
+    <<si::signed-integer-32>> = <<x::unsigned-integer-32>>
+  si
+  end
+
+  def twos_comp_64(x) do
+    <<si::signed-integer-64>> = <<x::unsigned-integer-64>>
+  si
+  end
+
 end
