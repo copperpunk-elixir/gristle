be311bb839b8279be61c2fd616f8d81f8f24ad19
Greg Gradwell <greghgradwell@live.com>
2020-11-07 14:12:49 -0800

    Added separate config.exs and fwup.conf files for each board, now that we need the Pi4 with a modified config.txt file.

---
+++ vcs/config/host.exs
@@ -0,0 +1,58 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+import Config
+
+# config :nerves, :firmware,
+  # fwup_conf: "config/fwup_rpi0.conf"
+
+config :vcs, target: Mix.target()
+
+# Customize non-Elixir parts of the firmware. See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+
+config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+config :shoehorn,
+  init: [:nerves_runtime, :vintage_net, :nerves_ssh],
+  app: Mix.Project.config()[:app]
+
+config :vintage_net,
+  persistence: VintageNet.Persistence.Null
+
+config :mdns_lite,
+  host: :hostname
+
+# Set the SOURCE_DATE_EPOCH date for reproducible builds.
+# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
+
+config :nerves, source_date_epoch: "1585927776"
+
+# Use Ringlogger as the logger backend and remove :console.
+# See https://hexdocs.pm/ring_logger/readme.html for more information on
+# configuring ring_logger.
+
+# config :logger, backends: [:console, RingLogger]
+config :logger, backends: [RingLogger]
+# config :logger, backends: []
+
+config :logger, RingLogger, max_size: 50_000
+
+config :logger,
+  level: :debug
+
+# config :logger, :console,
+#   format: "$time $metadata[$level] $levelpad$message\n",
+#   level: :debug,
+#   metadata: []
+
+config :ring_logger,
+  format: "$time $metadata[$level] $levelpad$message\n",
+  level: :debug,
+  metadata: []
+
+if Mix.target() != :host do
+  import_config "target.exs"
+end
--- vcs/lib/configuration/module/peripherals/uart.ex
+++ vcs/lib/configuration/module/peripherals/uart.ex
@@ -6,26 +6,34 @@ defmodule Configuration.Module.Peripherals.Uart do
     peripherals = Common.Utils.Configuration.get_uart_peripherals(subdirectory)
     Logger.debug("peripherals: #{inspect(peripherals)}")
     node_type = if Common.Utils.Configuration.is_hil?(), do: :hil, else: node_type
-    Enum.reduce(peripherals, %{}, fn (module, acc) ->
-      {module_key, module_config} = get_module_key_and_config_for_module(module, node_type)
+    Enum.reduce(peripherals, %{}, fn (name, acc) ->
+      peripheral_string = Atom.to_string(name)
+      [device, port] = String.split(peripheral_string, "_")
+      {module_key, module_config} = get_module_key_and_config(device, node_type, port)
       Map.put(acc, module_key, module_config)
     end)
   end
 
-  @spec get_module_key_and_config_for_module(atom(), atom()) :: tuple()
-  def get_module_key_and_config_for_module(module, node_type \\ nil) do
-    case module do
+  @spec get_module_key_and_config(atom(), atom(), binary()) :: tuple()
+  def get_module_key_and_config(device, node_type, port) do
+    Logger.debug("port: #{port}")
+    uart_port =
+      case port do
+        "usb" -> "usb"
+        port_num -> "ttyAMA#{String.to_integer(port_num)-2}"
+      end
+    case device do
       :Dsm -> {Command.Rx, get_dsm_rx_config()}
-      :FrskyRx -> {Command.Rx, get_frsky_rx_config()}
-      :FrskyServo -> {Actuation, get_actuation_config(module)}
-      :PololuServo -> {Actuation, get_actuation_config(module)}
-      :DsmRxFrskyServo -> {ActuationCommand, get_actuation_command_config(module)}
-      :FrskyRxFrskyServo -> {ActuationCommand, get_actuation_command_config(module)}
+      "FrskyRx" -> {Command.Rx, get_frsky_rx_config(uart_port)}
+      :FrskyServo -> {Actuation, get_actuation_config(device)}
+      :PololuServo -> {Actuation, get_actuation_config(device)}
+      :DsmRxFrskyServo -> {ActuationCommand, get_actuation_command_config(device)}
+      :FrskyRxFrskyServo -> {ActuationCommand, get_actuation_command_config(device)}
       :TerarangerEvo -> {Estimation.TerarangerEvo, get_teraranger_evo_config(node_type)}
       :VnIns -> {Estimation.VnIns, get_vn_ins_config(node_type)}
       :VnImu -> {Estimation.VnIns, get_vn_imu_config(node_type)}
-      :Xbee -> {Telemetry, get_telemetry_config(module)}
-      :Sik -> {Telemetry, get_telemetry_config(module)}
+      "Xbee" -> {Telemetry, get_telemetry_config(device)}
+      :Sik -> {Telemetry, get_telemetry_config(device)}
       :PwmReader -> {PwmReader, get_pwm_reader_config()}
     end
   end
@@ -34,19 +42,25 @@ defmodule Configuration.Module.Peripherals.Uart do
   def get_dsm_rx_config() do
     %{
       device_description: "CP2104",
-      baud: 115_200,
-      rx_module: :Dsm
+      rx_module: :Dsm,
+      port_options: [
+        speed: 115_200,
+        rx_framing_timeout: 11
+      ]
     }
   end
 
-  @spec get_frsky_rx_config() :: map()
-  def get_frsky_rx_config() do
+  @spec get_frsky_rx_config(binary()) :: map()
+  def get_frsky_rx_config(uart_port) do
     %{
-      device_description: "CP2104",
-      baud: 100_000,
-      stop_bits: 2,
-      rx_framing_timeout: 7,
-      rx_module: :Frsky
+      device_description: uart_port,
+      rx_module: :Frsky,
+      port_options: [
+        speed: 100000,
+        stop_bits: 2,
+        parity: :even,
+        rx_framing_timeout: 7
+      ]
     }
   end
 
@@ -137,16 +151,16 @@ defmodule Configuration.Module.Peripherals.Uart do
     }
   end
 
-  @spec get_telemetry_config(atom()) :: map()
-  def get_telemetry_config(module) do
-    {device_desc, baud} =
-      case module do
-        :Xbee -> {"FT231X", 57_600}
-        :Sik -> {"FT231X", 57_600}
+  @spec get_telemetry_config(binary()) :: map()
+  def get_telemetry_config(uart_port) do
+    device_desc =
+      case uart_port do
+        "usb" -> "FT231X"
+        port -> port
       end
     %{
       device_description: device_desc,
-      baud: baud,
+      baud: 57_600,
       fast_loop_interval_ms: Configuration.Generic.get_loop_interval_ms(:fast),
       medium_loop_interval_ms: Configuration.Generic.get_loop_interval_ms(:medium),
       slow_loop_interval_ms: Configuration.Generic.get_loop_interval_ms(:slow),
