8767dd3e2c8695e26408e696413599daea9855d1
Greg Gradwell <greghgradwell@live.com>
2020-11-15 11:14:07 -0800

    Added new battery test.

--- vcs/test/peripherals/i2c/battery/read_voltage_current_test.exs
+++ vcs/test/peripherals/i2c/battery/read_voltage_current_test.exs
@@ -27,6 +27,20 @@ defmodule Peripherals.I2c.Battery.ReadVoltageCurrentTest do
     assert battery.current_A > 1.0
   end
 
+  test "Interval read Atto90 Voltage/Current" do
+    device = "Atto90"
+    battery_type = "cluster"
+    battery_channel = 0
+    config = Configuration.Module.Peripherals.I2c.get_battery_config(String.to_atom(device), battery_type, battery_channel)
+    Logger.info("config: #{inspect(config)}")
+    # Peripherals.I2c.Health.Ina260.Operator.start_link(Map.get(config, Health.Ina260))
+    Peripherals.I2c.Health.Battery.Operator.start_link(config)
+    Process.sleep(1500)
+    battery = Peripherals.I2c.Health.Battery.Operator.get_battery(battery_type, battery_channel)
+    assert battery.voltage_V > 1.0
+    assert battery.current_A > 1.0
+  end
+
   # test "Read Ina219 Voltage/Current" do
   #   device = "Ina219"
   #   battery_type = "motor"
