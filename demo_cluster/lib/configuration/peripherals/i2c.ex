1843c1c4d901678778812f7c6c8981ec3808e0d8
Greg Gradwell <greghgradwell@live.com>
2021-03-09 07:13:27 -0800

    Changed LED addresses.

--- demo_cluster/lib/configuration/peripherals/i2c.ex
+++ demo_cluster/lib/configuration/peripherals/i2c.ex
@@ -1,4 +1,6 @@
 defmodule Configuration.Peripherals.I2c do
+  require Peripherals.I2c.Utils, as: PIU
+
   @spec get_config(binary()) :: list()
   def get_config(node_type) do
     {node, _ward, num_nodes} = Configuration.Cluster.get_node_and_ward(node_type)
@@ -10,9 +12,9 @@ defmodule Configuration.Peripherals.I2c do
       servo_output_sorter_interval_ms: Configuration.Generic.get_loop_interval_ms(:slow),
       mux_status_loop_interval_ms: Configuration.Generic.get_loop_interval_ms(:slow),
       leds: %{
-        self: 0x0A,
-        mux: 0x09,
-        servo_output: 0x08
+        self: PIU.self_led_address,
+        servo_output: PIU.servo_output_led_address,
+        mux: PIU.mux_led_address
       }
     ]
   end
--- demo_cluster/lib/peripherals/i2c/utils.ex
+++ demo_cluster/lib/peripherals/i2c/utils.ex
@@ -1,15 +1,15 @@
 defmodule Peripherals.I2c.Utils do
   @brightness 50
-  defmacro self_led_address, do: 0x08
+  defmacro self_led_address, do: 0x0A
   defmacro servo_output_led_address, do: 0x09
-  defmacro mux_led_address, do: 0x10
+  defmacro mux_led_address, do: 0x08
 
   def get_color_for_node_number(node) do
     case node do
       1 -> {@brightness,0,0} # RED
       2 -> {0, @brightness, 0} # GREEN
       3 -> {0, 0, @brightness} # BLUE
-      4 -> {0, @brightness, @brightness} # YELLOW?
+      4 -> {@brightness, @brightness, 0} # YELLOW?
       _other -> {0,0,0} #OFF
     end
   end
